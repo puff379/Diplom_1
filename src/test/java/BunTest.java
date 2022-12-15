0f24e6487c07473c728292aa43ea2db62153f5a6
ruslan.umov <ruslan.umov@ax-team.com>
2022-12-15 18:04:51 +0300

    Diplom_1 v2

--- src/test/java/BunTest.java
+++ src/test/java/BunTest.java
@@ -8,6 +8,7 @@ public class BunTest {
 
     private Bun bun;
 
+
     @Before
     public void setUp() {
         bun = new Bun("black bun", 100);
@@ -18,7 +19,34 @@ public class BunTest {
         String actualName = bun.getName();
         String expectedName = "black bun";
         assertEquals(expectedName, actualName);
+    }
 
+    @Test
+    public void emptyNameBun() {
+        bun = new Bun("", 100);
+        assertEquals(bun.getName(), "");
+    }
+
+    @Test
+    public void nullNameBun() {
+        bun = new Bun(null, 100);
+        assertEquals(bun.getName(), null);
+    }
+
+    @Test
+    public void longNameBun() {
+        String data = "a";
+        while (data.length() < 1000) {
+            data += "a";
+        }
+        bun = new Bun(data, 100);
+        assertEquals(bun.getName(), data);
+    }
+
+    @Test
+    public void cpecSymbolNameBun() {
+        bun = new Bun("#$%&", 100);
+        assertEquals(bun.getName(), "#$%&");
     }
 
     @Test
@@ -26,5 +54,41 @@ public class BunTest {
         Float actualPrice = bun.getPrice();
         Float expectedPrice = 100F;
         assertEquals(expectedPrice, actualPrice);
+    }
+
+    @Test
+    public void getBunMinusPrice() {
+        bun = new Bun("black bun", -100F);
+        assertEquals(bun.getPrice(), -100F, 1E-15);
+    }
+
+    @Test
+    public void getBunZeroPrice() {
+        bun = new Bun("black bun", 0);
+        assertEquals(bun.getPrice(), 0, 1E-15);
+    }
+
+    @Test
+    public void getMaxFloatZeroPrice() {
+        bun = new Bun("black bun", Float.MAX_VALUE);
+        assertEquals(bun.getPrice(), Float.MAX_VALUE, 1E-15);
+    }
+
+    @Test
+    public void getMaxIntZeroPrice() {
+        bun = new Bun("black bun", Integer.MAX_VALUE);
+        assertEquals(bun.getPrice(), Integer.MAX_VALUE, bun.getPrice() - Integer.MAX_VALUE);
+    }
+
+    @Test
+    public void getMinFloatZeroPrice() {
+        bun = new Bun("black bun", Float.MIN_VALUE);
+        assertEquals(bun.getPrice(), Float.MIN_VALUE, 1E-15);
+    }
+
+    @Test
+    public void getMinIntZeroPrice() {
+        bun = new Bun("black bun", Integer.MIN_VALUE);
+        assertEquals(bun.getPrice(), Integer.MIN_VALUE, 1E-15);
     }
 }
